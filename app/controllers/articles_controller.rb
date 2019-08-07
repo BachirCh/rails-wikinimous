49fac7357c459d97d40c198bdd9aea4dcb1e2c74
Bachir Cherrat <cherratbachir@gmail.com>
2019-08-07 10:54:24 +0100

    wikinimous

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show        # GET /restaurants/:id
+    @article = Article.find(params[:id])
+  end
+
+  def new           # GET /restaurants/new
+    @article = Article.new
+  end
+
+  def create        # POST /restaurants
+    @article = Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  def edit          # GET /restaurants/:id/edit
+  end
+
+  def update        # PATCH /restaurants/:id
+  end
+
+  def destroy       # DELETE /restaurants/:id
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
